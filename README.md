544a83147d10978689dc5f7a8097bbf506397295
AnixPasBesoin <anis.lounis@outlook.com>
2018-12-15 02:31:11 +0100

    Wrote the generic version

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ A generic parallel redcution using a single block (implemented in CUDA).
 
 ## TODOs
 - [x] push the integer version
+- [x] write a generic kernel (using C++ templates and functors)
 - [ ] write a shared memory version
-- [ ] write a generic kernel (using C++ templates and functors)
 - [ ] write an efficient segmented reduction
--- generic-reduction.cu
+++ generic-reduction.cu
@@ -1,13 +1,41 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <cuda.h>
+#include <device_functions.h>
+#include <cuda_runtime_api.h>
+
 #include <iostream>
 #include <assert.h>
 
-__global__ void reduce(int* v, const int n);
+template <typename T>
+struct BinaryAssociativeOperator {
+	__host__ __device__
+	virtual T operator() (const T left, const T right) const = 0;
+};
+
+template <typename T>
+struct Add : public BinaryAssociativeOperator<T>
+{
+	__host__ __device__
+	T operator() (const T left, const T right) const;
+};
+
+template <typename T>
+struct Max : public BinaryAssociativeOperator<T>
+{
+	__host__ __device__
+	T operator () (const T left, const T right) const;
+};
+
+template<typename T, typename Op>
+__global__ 
+void reduce(T * v, const int n , Op op);
 
 int main(int argc, char** argv) {
 
 	const int size = 8;
 
-	int h_v[size] = { 1, 2, 3, 4, 5, 6 , 7 , 8};
+	int h_v[size] = { 1, 2, 3, 4, 5, 6 , 7 , 8 };
 
 	int *d_v = 0;
 
@@ -16,43 +44,69 @@ int main(int argc, char** argv) {
 	cudaMemcpy(d_v, h_v, size * sizeof(int), cudaMemcpyHostToDevice);
 
 	dim3 grdDim(1, 1, 1);
-	dim3 blkDim(size/2, 1, 1);	
+	dim3 blkDim(size / 2, 1, 1);
 
-	reduce <<<grdDim, blkDim>>>(d_v, size);
+	// Max
+	Max<int> maxOp;
+	reduce<int, Max<int>> <<<grdDim, blkDim >> >(d_v, size, maxOp);
+	
 	cudaDeviceSynchronize();
 	
-	int sum;
-	cudaMemcpy(&sum, d_v, 1 * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaError_t error = cudaGetLastError();
+	if (error != cudaSuccess) {
+		printf("CUDA error: %s\n", cudaGetErrorString(error));
+		// system("pause"); // when using VisStudio
+		exit(-1);
+	}
+
+	int max;
+	cudaMemcpy(&max, d_v, 1 * sizeof(int), cudaMemcpyDeviceToHost);
 	cudaFree(d_v);
 
-	std::cout << "Somme ( ";
+	std::cout << "Max ( ";
 	for (int i = 0; i < size; i++) {
-		std::cout << h_v[i] << (i < size -1 ? " ," : " ) = ");
+		std::cout << h_v[i] << (i < size - 1 ? " ," : " ) = ");
 	}
-	std::cout << sum << std::endl;
+	std::cout << max << std::endl;
+
+	// system("pause"); // when using VisStudio
 
 	return 0;
 }
 
+template<typename T>
+__host__ __device__
+T Add<T>::operator() (const T left, const T right) const {
+	return left + right;
+}
 
-__global__ void reduce(int *v, const int n)
+template<typename T>
+__host__ __device__
+T Max<T>::operator() (const T left, const T right) const {
+	return left > right ? left : right;
+}
+
+
+template<typename T, typename Op>
+__global__ 
+void reduce(T *v, const int n, Op op)
 {
-	
+
 	// Thread Id
-        const int tId = threadIdx.x;
+	const unsigned int tId = threadIdx.x;
 	// Initial thread Count
-	const int initialThreadCount = blockDim.x;
-	
+	const unsigned int initialThreadCount = blockDim.x;
+
 	// assert(initialThreadCount >= n);
-	
+
 	for (int threadCount = initialThreadCount, stepSize = 1;
 		threadCount > 0;
-		threadCount /= 2, stepSize *=2) {
-		
+		threadCount /= 2, stepSize *= 2) {
+
 		if (tId < threadCount) {
 			int indiceDroite = tId * stepSize * 2;
 			int indiceGauche = indiceDroite + stepSize;
-			v[indiceDroite] += v[indiceGauche];
+			v[indiceDroite] = op(v[indiceDroite], v[indiceGauche]);
 		}
 		__syncthreads();
 	}
